36118c0d43c7482d89fd747bc81c88cb25319bfb
JLSed <seddd001@gmail.com>
2024-05-13 21:40:19 +0800

    clean up comment mess

--- MultiThreadedServer.java
+++ MultiThreadedServer.java
@@ -4,17 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MultiThreadedServer {
-    private static Map<String, PrintWriter> clientsMap = new HashMap<>(); // location for all the connected clients
+    // location for all the connected clients
+    private static Map<String, PrintWriter> clientsMap = new HashMap<>(); 
 
     public static void main(String[] args) throws IOException {
-        int port = 8080; // Port to listen on
+        int port = 8080;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server started on port " + port);
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-            // Start a new thread to handle client communication
+            // handle client communication
             new Thread(() -> {
                 try {
                     handleClient(clientSocket);
@@ -48,9 +49,6 @@ public class MultiThreadedServer {
                     message = inputLine;
                     sendMessageToClient(currentInteraction, message);
                 }
-                // Process the input if needed
-                // Send a message to the specific client
-
             }
 
         } finally {
